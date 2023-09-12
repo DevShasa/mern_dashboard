580af5917c3eea819f808017aaf1b0a71ff81581
DevShasa <shasa.thuo@gmail.com>
2023-09-12 05:01:41 +0300

    feat: added navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,29 @@
-
+import { useMemo } from "react"
+import { CssBaseline, ThemeProvider } from "@mui/material"
+import {createTheme} from "@mui/material/styles" // this is like a css refix
+import { themeSettings } from "./theme"
+import { useSelector } from "react-redux"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
+import Layout from "./scenes/layout"
+import Dashboard from "./scenes/dashboard"
 
 function App() {
-
+  const mode = useSelector((state)=> state.theme.mode)
+  const theme = useMemo(()=> createTheme(themeSettings(mode)), [mode])
   return (
-    <>
-      Wolan
-    </>
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Navigate to="/dashboard" replace/>}/> when user hits / redirect to /dashboard
+              <Route path="/dashboard" element={<Dashboard />}/>
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
   )
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { configureStore } from '@reduxjs/toolkit';
+import themeReducer from "./redux/themeslice.js";
+import { Provider } from 'react-redux';
+
+
+const store = configureStore({
+  reducer:{
+    theme: themeReducer
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
