a5158e732049eac0d58ec418b5f91d0b6bbaf785
DevShasa <shasa.thuo@gmail.com>
2023-12-15 19:46:58 +0300

    get gepgraphy endpoint

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -2,7 +2,7 @@ import Product from "../models/Product.js";
 import ProductStat from "../models/ProductStat.js";
 import User from "../models/User.js";
 import Transaction from "../models/Transaction.js";
-//import getCountryIso3 from "country-iso-2-to-3"
+import getCountryIso3 from "country-iso-2-to-3"
 
 export const getProducts = async (req, res)=>{
     console.log("WOLLLANDE ")
@@ -87,7 +87,42 @@ export const getGeography = async (req, res)=>{
     try {
         const users = await User.find();
         
+        const mappedLocations = users.reduce((acc, {country})=>{
+            const countryISO3 = getCountryIso3(country)
+            if(!acc[countryISO3]){
+                acc[countryISO3] = 0
+            }
+            
+            acc[countryISO3]++
 
+            return acc
+            //  will return something like this 
+            // {
+            //     "CHN": 54,
+            //     "IDN": 35,
+            //     "POL": 11,
+            //     "THA": 6,
+            //     "RUS": 24,
+            // }
+        },{})
+
+
+        const formatedLocations = Object.entries(mappedLocations).map(([key, value])=>{
+            return {id:key, value}
+        })
+
+        // THis is what the map function above does         
+        // [
+        //     {
+        //         "id": "CHN",
+        //         "value": 54
+        //     },
+        //     {
+        //         "id": "IDN",
+        //         "value": 35
+        //     },
+        // ]
+        res.status(200).json(formatedLocations)
 
     } catch (error) {
         res.status(404).json({message: error.message})
