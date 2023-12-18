d6d85adfe03fc78ce4bce656633b9e555f92b03e
DevShasa <shasa.thuo@gmail.com>
2023-12-18 18:34:08 +0300

    geography page

--- client/src/scenes/geography/index.jsx
+++ client/src/scenes/geography/index.jsx
@@ -1,12 +1,33 @@
 import { useGetGeographyQuery } from "../../redux/api";
-
+import { geoData } from "../../redux/geoData";
+import Header from "../../components/Header";
+import { ResponsiveChoropleth } from "@nivo/geo";
+import { Box, useTheme } from "@mui/material";
 
 const Geography = () => {
 	const { data, isLoading } = useGetGeographyQuery();
+	const theme = useTheme()
+
+
 
 	console.log("THis is the data >>>", data)
 
-	return <div>Geography</div>;
+	return (
+		<Box m="1.5rem 2.5rem">
+			<Header title="GEOGRAPHY" subtitle="Find where your users are located"/>
+			<Box
+				mt="40px"
+				height="75vh"
+				border={`1px solid ${theme.palette.secondary[200]}`}
+				borderRadius="4px"
+			>
+				{data 
+					? <>Chloropleth</>
+					: <div>Loading ...</div>
+				}
+			</Box>
+		</Box>
+	)
 };
 
 export default Geography;
