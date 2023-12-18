f469ae2bb9bd540c597e79a7906ab49cbe84b961
DevShasa <shasa.thuo@gmail.com>
2023-12-18 18:10:22 +0300

    added geodata

--- client/src/scenes/geography/index.jsx
+++ client/src/scenes/geography/index.jsx
@@ -1,7 +1,11 @@
 import { useGetGeographyQuery } from "../../redux/api";
 
+
 const Geography = () => {
 	const { data, isLoading } = useGetGeographyQuery();
+
+	console.log("THis is the data >>>", data)
+
 	return <div>Geography</div>;
 };
 
